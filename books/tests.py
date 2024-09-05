7f6388f2b31dec36fc222fb4504b2e351018883e
Eric Gotkowski <egotkowski@gmail.com>
2024-09-04 22:40:28 -0500

    adding basic tests

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Book
+
+class BookTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.book = Book.objects.create(
+            title="A Test Title",
+            subtitle="A test subtitle",
+            author="Testing Testerson",
+            isbn="123456789",
+        )
+
+    def test_book_content(self):
+        self.assertEqual(self.book.title, "A Test Title")
+        self.assertEqual(self.book.subtitle, "A test subtitle")
+        self.assertEqual(self.book.author, "Testing Testerson")
+        self.assertEqual(self.book.isbn, "123456789")
+
+    def test_book_listview(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "A test subtitle")
+        self.assertTemplateUsed(response, "books/book_list.html")
